ebea79d9c80d2a3fa2b3ce509cca5a9297f8ca0f
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2023-01-26 19:22:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import { BrowserRouter, Route,Routes } from "react-router-dom";
+import "./App.css"
+import NavBar from "./Components/Navbar";
+import Contact from "./Components/Contact";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Work from "./Components/Work";
+import Portfolio from "./Components/Portfolio";
+import Profile from "./Components/Profile";
+import SocialMedia from "./Components/SocialMedia";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component  {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/socialMedia" element={<SocialMedia />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
