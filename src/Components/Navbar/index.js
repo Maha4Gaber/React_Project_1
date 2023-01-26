b04cafe0438b3f05848b1970986be08fc71b9578
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2023-01-26 19:47:24 +0200

    update

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 // import { Link } from "react-router-dom";
 
 // import styled from "styled-components";
-import {NaveSection ,Logo ,LogoText ,Ullist,NavbarLink ,Listitem,ListitemLink} from "./style.js"
+import {NaveSection ,Logo ,LogoText ,Ullist,NavbarLink ,Listitem} from "./style.js"
 
 const NavBar = () => {
   return (
